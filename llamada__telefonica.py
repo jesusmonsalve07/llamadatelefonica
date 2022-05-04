02449630634773b9271721311cec38993ef948ab
jesusmonsalve07 <jesus2224505@correo.uis.edu.co>
2022-05-03 22:06:46 -0500

    desarrollo ejercicio llamada telefonica

---
+++ llamada__telefonica.py
@@ -0,0 +1,23 @@
+"Programa para calcular el valor de una llamada telefonica"
+
+print("""---------------------------------------""")
+print("""calcular el valor de la llamada--------""")
+print("""---------------------------------------""")
+
+#input
+
+int(input(" Digite el numero de minutos:" )) 
+
+#processing
+
+if x<= 3:
+    msj= "el valor es de 300"
+
+else 
+    msj= 300 + (50 * (x-3)) 
+
+#output
+
+print("el costo es :" + str(msj))  
+
+
